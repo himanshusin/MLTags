759453fb864e02e87a7355caba2d094aecae4914
himanshusin <himanshusin@gmail.com>
2019-10-17 14:53:16 -0500

    -- Parameterized encoding is also working

--- 01. Data Generator.R
+++ 01. Data Generator.R
@@ -15,34 +15,70 @@ faker_local <- faker_global$Faker(locale = generator_language_encoding)
 #faker_global$config$PROVIDERS
 #faker_global$config$AVAILABLE_LOCALES
 
-faker_local$address()
-
-
-generate_data <- function (nrow) {
-  
+generate_data <- function (nrow, data_class, data_value) {
+    
   lst <- list()  
   for (i in 1:nrow) {
     
     
     value <- faker_local$name()
     class <- 'faker$name'
+    
+    
+    
     lst[[i]]   <- list (class, value )
     
-   # data<- as.data.frame(lst)
-    
         }
-    return(lst)
+   return(lst)
   }
   
   
   
-a<- generate_data(10)
+a<- generate_data(nrow=10, data_class=name , data_value=faker_local$name())
 a
 
 
-a[[10]][[2]]
 
 b<- do.call(rbind,a)
+b
+
+
+
+###------------------ Paramertized  encoding 
+
+
+available_locales = list("ru_RU","hi_IN","EN")
+
+
+faker_global <-import('faker')
+#globalenv(faker_global)
+
+generate_data <- function (encoding , nrow, data_class, data_value) {
+faker_local <- faker_global$Faker(locale = encoding)
+lst <- list()  
+
+df <- data.frame(data_class=character(),
+             data_value=character() 
+                 ) 
+
+for (i in 1:nrow) {
+    
+    
+    value <- faker_local$name()
+    class <- data_class
+    lst[[i]]   <- list (class, value )
+    
+}
+  df <- as.data.frame(do.call(rbind,lst))
+  names(df)[1]<-'data_class'
+  names(df)[2]<-'data_value'
+  return(df)
+}
+
+
+hindi_address <- generate_data(encoding = 'hi_IN',nrow=10, data_class="name" , data_value=faker_local$name())
+english_address <- generate_data(encoding = 'EN',nrow=10, data_class="age" , data_value=faker_local$address())
+df <- rbind ( hindi_address, english_address)
 
 
 

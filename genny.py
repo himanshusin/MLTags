f40a3749aa5b1ad293dec77783e50a264b9934e0
himanshusin <himanshusin@gmail.com>
2019-10-22 15:54:47 -0500

    --genny updated

--- genny.py
+++ genny.py
@@ -5,42 +5,44 @@ Created on Thu Oct 17 17:36:52 2019
 @author: hisinha
 """
 
-from faker import Faker 
-fake=Faker()
-
-fake.name()
-
-
-
-
+import os
+import sys
+os.chdir('C:/01 Solution Dev/MoonShine/MLTags')
 from faker import Faker 
 import pandas as pd
-#fake= Faker()
+import uuid
+#fake=Faker()
+#type(dir(fake))
 
-def producer(loc ,f, lst):
-    
-    fake= Faker(loc)   
-    
-    output = [{ 
-            'data_value' : getattr(fake,f)()} 
-    for x in range(n)]               
-    final=pd.DataFrame(output)
-    return final
+def producer(loc ,flds, n): 
+    #        check if list1 contains all elements in list2
+    fake= Faker(loc)
+    result =  all(elem in dir(fake)  for elem in flds)
+    if result:
+        print("Yes, all requested fields are valid !")
+        df=pd.DataFrame()
+        for x in range(len(flds)):
+            for y in range (n):
+                df= df.append({ 'uuid': uuid.uuid1(), 'data_value' : getattr(fake,flds[x])(),'data_locale': loc,'data_class': str(flds[x])} , ignore_index=True )
+        print("job completed successfully..\N{Thumbs Up Sign}")
+        df.to_csv(r'dummy_data.csv')
+        return df
+    else:
+        print("Some of the fields doesn't contains all valid elements, terminating....  \N{Weary Face}" )
+        
+
+a= producer ( 'EN',['name'],5)
+
+fake = Faker()
+fake.profile()
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
 
 
-df = producer('ru_RU',lst = ['name','address'],5)
 
 
 
 
-
-
-
-
-import pandas as pd
-df1 = pd.DataFrame({'col1': [1,2], 'col2':[3,4]})
-df2 = pd.DataFrame({'col1': [5,6], 'col2':[7,8]})
-print(df1)
-print(df2)
-print(pd.concat([df1, df2]))
